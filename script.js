f4b79a4c1bf26a192b16104a749703ceb442f42d
Jack <jackrileynicosia@yahoo.com>
2023-04-12 14:37:42 -0400

    added a clear function

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let calculations = document.querySelector("#calculations");
 function results(input){
    document.getElementById("calculations").insertAdjacentHTML('beforeend', (input));
 }
+
+
+function clearResults(){
+    document.getElementById("calculations").innerHTML = ""; 
+}
