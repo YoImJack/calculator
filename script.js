26ce4b0d45739178e115cab3fd67969690f13a08
Jack <jackrileynicosia@yahoo.com>
2023-04-18 15:56:58 -0400

    able to use multi-digit numbers

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 let firstNumber = null;
 let secondNumber = null;
 let operator = null;
+let result = null;
 
 function results(input) {
   document.getElementById("calculations").insertAdjacentHTML('beforeend', input);
+  const inputAsString = `${input}`; // cast to a string
 
   if (operator === null) {
+    // If operator is null, update firstNumber
     if (firstNumber === null) {
-      // If firstNumber is null, set it to the input
-      firstNumber = input;
-    } else if (input !== '+' && input !== '-' && input !== '*' && input !== '/') {
-      // Otherwise, concatenate the input to the existing firstNumber
-      firstNumber += input;
+      firstNumber = inputAsString;
+    } else {
+      firstNumber += inputAsString;
     }
   } else {
+    // If operator is not null, update secondNumber
     if (secondNumber === null) {
-      // If secondNumber is null, set it to the input
-      secondNumber = input;
-    } else if (input !== '+' && input !== '-' && input !== '*' && input !== '/') {
-      // Otherwise, concatenate the input to the existing secondNumber
-      secondNumber += input;
+      secondNumber = inputAsString;
+    } else {
+      secondNumber += inputAsString;
     }
   }
 }
@@ -29,9 +29,22 @@ function clearResults() {
   firstNumber = null;
   secondNumber = null;
   operator = null;
+  result = null;
 }
 
 function calculateResult() {
+  console.log(firstNumber, secondNumber);
+  if (firstNumber === null || secondNumber === null) {
+    return null;
+  }
+
+  const num1 = Number(firstNumber);
+  const num2 = Number(secondNumber);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    return null;
+  }
+
   switch (operator) {
     case "+":
       return Number(firstNumber) + Number(secondNumber);
@@ -47,15 +60,17 @@ function calculateResult() {
 }
 
 function enterButton() {
-  // Calculate the result and update the calculations element
-  const result = calculateResult();
-  if (result !== null) {
-    document.getElementById("calculations").innerHTML = result;
-    // Set firstNumber to the result, and reset secondNumber and operator
-    firstNumber = result;
-    secondNumber = null;
-    operator = null;
+  if (result === null) {
+    // Calculate the result and update the calculations element
+    const result = calculateResult();
+    if (result !== null) {
+      document.getElementById("calculations").innerHTML = result;
+    }
   }
+  // Set firstNumber to the result, and reset secondNumber and operator
+  firstNumber = result;
+  secondNumber = null;
+  operator = null;
 }
 
 function setOperator(op) {
@@ -71,5 +86,8 @@ function setOperator(op) {
       secondNumber = null;
       operator = op;
     }
+  } else if (firstNumber !== null && operator !== null && secondNumber === null) {
+    operator = op;
+    document.getElementById("calculations").innerHTML = firstNumber + op;
   }
 }
